283f8d68ceb825b1cb9522cf2327bc591ebdfc6c
Joshvu93 <joshvu93@gmail.com>
2021-10-15 12:53:16 -0700

    added media upload and messaging feature

--- src/index.js
+++ src/index.js
@@ -1,71 +1,73 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import Login from './components/Auth/Login';
-import Register from './components/Auth/Register';
-import registerServiceWorker from './registerServiceWorker';
-import firebase from './firebase';
-import Spinner from './Spinner';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+import Spinner from "./Spinner";
+import registerServiceWorker from "./registerServiceWorker";
+import firebase from "./firebase";
 
-import 'semantic-ui-css/semantic.min.css';
+import "semantic-ui-css/semantic.min.css";
 
-import { 
-  BrowserRouter as Router, 
-  Switch, 
-  Route, 
-  withRouter 
-} from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter
+} from "react-router-dom";
 
-import { createStore } from'redux';
-import { Provider, connect } from 'react-redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducer from './reducers';
-import { setUser, clearUser } from './actions';
+import { createStore } from "redux";
+import { Provider, connect } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./reducers";
+import { setUser, clearUser } from "./actions";
 
 const store = createStore(rootReducer, composeWithDevTools());
 
 class Root extends React.Component {
   componentDidMount() {
-    console.log(this.props.isLoading)
     firebase.auth().onAuthStateChanged(user => {
-      if(user) {
+      if (user) {
         // console.log(user);
         this.props.setUser(user);
         this.props.history.push("/");
       } else {
-        this.props.history.push('/login');
+        this.props.history.push("/login");
         this.props.clearUser();
       }
     });
   }
+
   render() {
-  return this.props.isLoading ? <Spinner /> : (
+    return this.props.isLoading ? (
+      <Spinner />
+    ) : (
       <Switch>
-        <Route exact path='/' component={App} />
-        <Route path='/login' component={Login} />
-        <Route path='/register' component={Register} />
+        <Route exact path="/" component={App} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
       </Switch>
     );
   }
 }
 
-const mapStateToProps = state => ({
+const mapStateFromProps = state => ({
   isLoading: state.user.isLoading
 });
 
 const RootWithAuth = withRouter(
   connect(
-    mapStateToProps,
-    { setUser, clearUser}
-    )(Root)
-    );
+    mapStateFromProps,
+    { setUser, clearUser }
+  )(Root)
+);
 
 ReactDOM.render(
-<Provider store={store}>
-  <Router>
-    <RootWithAuth />
-  </Router> 
-</Provider>,
-  
-  document.getElementById('root'));
+  <Provider store={store}>
+    <Router>
+      <RootWithAuth />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,13 @@
-import { combineReducers } from 'redux';
-import * as actionTypes from '../actions/types';
+import { combineReducers } from "redux";
+import * as actionTypes from "../actions/types";
 
-const initalUserState = {
+const initialUserState = {
   currentUser: null,
   isLoading: true
 };
 
-const user_reducer = (state = initalUserState, action) => {
-  switch(action.type){
+const user_reducer = (state = initialUserState, action) => {
+  switch (action.type) {
     case actionTypes.SET_USER:
       return {
         currentUser: action.payload.currentUser,
@@ -15,8 +15,24 @@ const user_reducer = (state = initalUserState, action) => {
       };
     case actionTypes.CLEAR_USER:
       return {
-        ...initalUserState,
+        ...state,
         isLoading: false
+      };
+    default:
+      return state;
+  }
+};
+
+const initialChannelState = {
+  currentChannel: null
+};
+
+const channel_reducer = (state = initialChannelState, action) => {
+  switch (action.type) {
+    case actionTypes.SET_CURRENT_CHANNEL:
+      return {
+        ...state,
+        currentChannel: action.payload.currentChannel
       }
     default:
       return state;
@@ -24,7 +40,8 @@ const user_reducer = (state = initalUserState, action) => {
 }
 
 const rootReducer = combineReducers({
-  user: user_reducer
+  user: user_reducer,
+  channel: channel_reducer
 });
 
 export default rootReducer;
